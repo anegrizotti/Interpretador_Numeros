eb1f5cce5ea020441a3a46d768a85c25435f8917
Ane Grizotti <algrizotti@gmail.com>
2022-02-17 22:58:42 -0300

    Entrada alterada para 4 fileiras de números em vez de 1.

--- Interpretador_Numeros.ConsoleApp/Program.cs
+++ Interpretador_Numeros.ConsoleApp/Program.cs
@@ -6,90 +6,115 @@ namespace Interpretador_Numeros.ConsoleApp
     {
         static void Main(string[] args)
         {
+            string numeros = " __  __  __  __  __      __  __     \n" +
+                             "|__ |__    ||__||__|  |  __| __||__|\n" +
+                             " __||__|   ||__| __|  | |__  __|   |\n" +
+                             "                                    \n" +
+                             " __      __  __      __  __  __  __ \n" +
+                             " __||__|   ||__|  |  __||__ |__ |__|\n" +
+                             " __|   |   ||__|  | |__  __||__| __|\n" +
+                             "                                    \n" +
+                             " __  __  __  __  __      __  __     \n" +
+                             " __| __||__ |__ |__||__|   ||__|  | \n" +
+                             " __||__  __||__| __|   |   ||__|  | \n" +
+                             "                                    \n" +
+                             " __  __  __  __  __  __      __     \n" +
+                             "|__| __||__ |__    ||__|  | |__||__|\n" +
+                             "|__||__  __||__|   ||__|  |  __|   |\n" +
+                             "                                    ";
 
-            string numeros = "     __  __      __  __  __  __  __  __ \n" +
-                             "  |  __| __||__||__ |__    ||__||__||  |\n" +
-                             "  | |__  __|   | __||__|   ||__| __||__|\n";
 
-            int[] numeroResultado = new int[10];
+            int[] numeroResultado = new int[9];
+            string[] primeiraLinha = new string[4];
+            string[] segundaLinha = new string[4];
+            string[] terceiraLinha = new string[4];
+            string[] quartaLinha = new string[4];
             string[] substringPrimeiraLinha = new string[10];
             string[] substringSegundaLinha = new string[10];
             string[] substringTerceiraLinha = new string[10];
-            int contX = 0;
-            int contY = 4;
+            int contX;
+            int contY;
 
             System.IO.StringReader leitor = new System.IO.StringReader(numeros);
 
-            string primeiraLinha = leitor.ReadLine();
-
-            string segundaLinha = leitor.ReadLine();
-
-            string terceiraLinha = leitor.ReadLine();
-
-            for (int i = 0; i < 10; i++) {
-
-                substringPrimeiraLinha[i] = primeiraLinha.Substring(contX, contY);
-                substringSegundaLinha[i] = segundaLinha.Substring(contX, contY);
-                substringTerceiraLinha[i] = terceiraLinha.Substring(contX, contY);
-
-                if (substringPrimeiraLinha[i] == "    " && substringSegundaLinha[i] == "  | " && substringTerceiraLinha[i] == "  | ")
-                {
-                    numeroResultado[i] = 1;
-
-                }
-
-                else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == " __|" && substringTerceiraLinha[i] == "|__ ")
-                {
-                    numeroResultado[i] = 2;
-                }
-
-                else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == " __|" && substringTerceiraLinha[i] == " __|")
-                {
-                    numeroResultado[i] = 3;
-                }
-
-                else if (substringPrimeiraLinha[i] == "    " && substringSegundaLinha[i] == "|__|" && substringTerceiraLinha[i] == "   |")
-                {
-                    numeroResultado[i] = 4;
-                }
-
-                else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == "|__ " && substringTerceiraLinha[i] == " __|")
-                {
-                    numeroResultado[i] = 5;
-                }
-
-                else if (substringPrimeiraLinha[i] == (" __ ") && substringSegundaLinha[i] == "|__ " && substringTerceiraLinha[i] == "|__|")
-                {
-                    numeroResultado[i] = 6;
-                }
-
-                else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == "   |" && substringTerceiraLinha[i] == "   |")
-                {
-                    numeroResultado[i] = 7;
-                }
-
-                else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == "|__|" && substringTerceiraLinha[i] == "|__|")
-                {
-                    numeroResultado[i] = 8;
-                }
-
-                else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == "|__|" && substringTerceiraLinha[i] == " __|")
-                {
-                    numeroResultado[i] = 9;
-                }
-
-                else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == "|  |" && substringTerceiraLinha[i] == "|__|")
-                {
-                    numeroResultado[i] = 0;
-                }
-
-                contX = contX + 4;
-
-            }
-
-            for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 4; j++)
             {
-                Console.Write(numeroResultado[i]);
+                contX = 0;
+                contY = 4;
+                primeiraLinha[j] = leitor.ReadLine();
+                segundaLinha[j] = leitor.ReadLine();
+                terceiraLinha[j] = leitor.ReadLine();
+                quartaLinha[j] = leitor.ReadLine();
+
+                for (int i = 0; i < 9; i++)
+                {
+
+                    substringPrimeiraLinha[i] = primeiraLinha[j].Substring(contX, contY);
+                    substringSegundaLinha[i] = segundaLinha[j].Substring(contX, contY);
+                    substringTerceiraLinha[i] = terceiraLinha[j].Substring(contX, contY);
+
+                    if (substringPrimeiraLinha[i] == "    " && substringSegundaLinha[i] == "  | " && substringTerceiraLinha[i] == "  | ")
+                    {
+                        numeroResultado[i] = 1;
+
+                    }
+
+                    else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == " __|" && substringTerceiraLinha[i] == "|__ ")
+                    {
+                        numeroResultado[i] = 2;
+                    }
+
+                    else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == " __|" && substringTerceiraLinha[i] == " __|")
+                    {
+                        numeroResultado[i] = 3;
+                    }
+
+                    else if (substringPrimeiraLinha[i] == "    " && substringSegundaLinha[i] == "|__|" && substringTerceiraLinha[i] == "   |")
+                    {
+                        numeroResultado[i] = 4;
+                    }
+
+                    else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == "|__ " && substringTerceiraLinha[i] == " __|")
+                    {
+                        numeroResultado[i] = 5;
+                    }
+
+                    else if (substringPrimeiraLinha[i] == (" __ ") && substringSegundaLinha[i] == "|__ " && substringTerceiraLinha[i] == "|__|")
+                    {
+                        numeroResultado[i] = 6;
+                    }
+
+                    else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == "   |" && substringTerceiraLinha[i] == "   |")
+                    {
+                        numeroResultado[i] = 7;
+                    }
+
+                    else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == "|__|" && substringTerceiraLinha[i] == "|__|")
+                    {
+                        numeroResultado[i] = 8;
+                    }
+
+                    else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == "|__|" && substringTerceiraLinha[i] == " __|")
+                    {
+                        numeroResultado[i] = 9;
+                    }
+
+                    else if (substringPrimeiraLinha[i] == " __ " && substringSegundaLinha[i] == "|  |" && substringTerceiraLinha[i] == "|__|")
+                    {
+                        numeroResultado[i] = 0;
+                    }
+
+                    contX = contX + 4;
+
+                }
+
+                for (int i = 0; i < 9; i++)
+                {
+                    Console.Write(numeroResultado[i]);
+                }
+
+                Console.WriteLine();
+
             }
 
         }   
